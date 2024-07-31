798a0304378b4086dec45017f41d0d722ca54c04
Sayed Hasan <94556687+emonislive@users.noreply.github.com>
2024-07-31 23:15:07 +0600

    AC Please

---
+++ c++/ACPLZ.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+#define endl '\n'
+int main() {
+	ios::sync_with_stdio(false); cin.tie(NULL);
+	int a;
+	cin >> a;
+	if (a > 30)
+	    cout << "YES" << endl;
+	else
+	    cout << "NO" << endl;
+    
+    return 0;
+}
